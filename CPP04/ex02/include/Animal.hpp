19110ecd88e24b4f11b2340c59b06301bfef900a
yo <yo@anaalejandra.com>
2025-06-25 20:32:30 +0000

    [CPP04] Finished Exercise 02

---
+++ CPP04/ex02/include/Animal.hpp
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Animal.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ana-cast <ana-cast@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/25 12:35:04 by ana-cast          #+#    #+#             */
+/*   Updated: 2025/06/25 16:09:31 by ana-cast         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ANIMAL_HPP
+# define ANIMAL_HPP
+
+# include <iostream>
+
+class Animal
+{
+    protected:
+        std::string type;
+
+    public:
+        Animal(std::string type);
+        Animal();
+        virtual ~Animal();
+        Animal(const Animal& other);
+        Animal& operator=(const Animal& other);
+
+        virtual void makeSound() const = 0;
+        std::string getType() const;
+};
+
+#endif /* ANIMAL_HPP */
---
+++ CPP04/ex02/include/WrongCat.hpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   WrongCat.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ana-cast <ana-cast@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/25 12:35:04 by ana-cast          #+#    #+#             */
+/*   Updated: 2025/06/25 13:05:38 by ana-cast         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef WRONGCAT_HPP
+# define WRONGCAT_HPP
+
+# include "WrongAnimal.hpp"
+
+class WrongCat : public WrongAnimal
+{
+    public:
+        WrongCat();
+        ~WrongCat();
+        WrongCat(const WrongCat& other);
+        WrongCat& operator=(const WrongCat& other);
+
+        void makeSound() const;
+};
+
+#endif /* WRONGCAT_HPP */
